b1bfd88e1924d79c43a2086791b8d2d002e058bb
raydeal <alnw@interia.eu>
2021-07-22 11:22:10 +0200

    adding, editing user task

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import UserTaskListView
+from .views import UserTaskListView, UserTaskCreateView, UserTaskUpdateView
 
 urlpatterns = [
-     path('', UserTaskListView.as_view()),
+    path("", UserTaskListView.as_view(), name='task-list'),
+    path("task/add/", UserTaskCreateView.as_view(), name='task-add'),
+    path("task/<int:pk>/", UserTaskUpdateView.as_view(), name='task-update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,21 @@
 # from django.shortcuts import render
-
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import UserTask
 
 class UserTaskListView(ListView):
     model = UserTask
+
+
+class UserTaskCreateView(CreateView):
+    model = UserTask
+    success_url = reverse_lazy('task-list')
+    fields = ['user', 'location', 'title', 'description']
+
+
+class UserTaskUpdateView(UpdateView):
+    model = UserTask
+    success_url = reverse_lazy('task-list')
+    fields = ['user', 'location', 'status', 'title', 'description']
